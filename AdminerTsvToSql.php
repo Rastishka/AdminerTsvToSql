865551f223d2ff0f0a6f81612a6e5e1f6aac80a6
Rastishka <zenin.ilya@gmail.com>
2017-06-04 13:55:29 +0300

    Add files via upload

---
+++ AdminerTsvToSql.php
@@ -0,0 +1,119 @@
+<?php
+
+
+class AdminerTsvToSql
+{
+    function head() {
+        if (!isset($_GET['sql'])) {
+            return;
+        }
+        ?>
+        <script type="text/javascript">
+            document.addEventListener("DOMContentLoaded", function (event) {
+                var sqlarea = document.getElementsByClassName("sqlarea")[0];
+
+                sqlarea.addEventListener('paste', function (e) {
+                    var clipboardData, pastedText;
+                    clipboardData = e.clipboardData || window.clipboardData;
+                    pastedText = clipboardData.getData('Text');
+
+                    var n = pastedText.search("\t");
+                    var lineCount = (pastedText.match(/\r\n/g) || []).length;
+                    var tabCount = (pastedText.match(/\t/g) || []).length;
+
+                    if ((lineCount >= 1) &&
+                        (tabCount >= 2))
+                    {
+                        var lines = pastedText.split("\r\n");
+
+                        // check if valid
+                        var headerColumnsLength = lines[0].split("\t").length;
+                        for (var i = 1; i < lines.length; i++) {
+                            if (lines[i] != '') {
+                                if (headerColumnsLength != lines[i].split("\t").length) {
+                                    return;
+                                }
+                            }
+                        }
+
+                        if (confirm('Convert clipboard TSV to SQL query?')) {
+                            var sqlData = [];
+
+                            for (var i = 1; i < lines.length; i++) {
+                                if (lines[i] != '') {
+                                    sqlData.push(
+                                        "('" +
+                                        lines[i]
+                                            .split("\t")
+                                            .map(
+                                                function (s) {
+                                                    if ((s.length >= 2) &&
+                                                        (s.charAt(0) == '"') &&
+                                                        (s.charAt(s.length - 1) == '"'))
+                                                    {
+                                                        s = s.substring(1, s.length - 1);
+                                                    }
+
+                                                    return s.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
+                                                        switch (char) {
+                                                            case "\0":
+                                                                return "\\0";
+                                                            case "\x08":
+                                                                return "\\b";
+                                                            case "\x09":
+                                                                return "\\t";
+                                                            case "\x1a":
+                                                                return "\\z";
+                                                            case "\n":
+                                                                return "\\n";
+                                                            case "\r":
+                                                                return "\\r";
+                                                            case "\"":
+                                                            case "'":
+                                                            case "\\":
+                                                            case "%":
+                                                                return "\\"+char; // prepends a backslash to backslash, percent,
+                                                                                  // and double/single quotes
+                                                        }
+                                                    });
+                                                }
+                                            )
+                                            .join("', '") +
+                                        "')"
+                                    );
+                                }
+                            }
+
+                            var sql =
+                                'INSERT INTO `INPUT_TABLE_NAME` \n' +
+                                '   (`' +
+                                lines[0]
+                                    .split("\t")
+                                    .map(
+                                        function (s) {
+                                            return s.trim()
+                                        }
+                                    )
+                                    .join('`, `') +
+                                '`)\n' +
+                                'VALUES \n' +
+                                '   ' + sqlData.join(',\n   ') +
+                                ';';
+
+                            setTimeout(function () {
+                                sqlarea.innerHTML = '';
+                                document.execCommand("insertText", false, sql);
+
+                                var eventClick = new Event('keyup');
+                                sqlarea.dispatchEvent(eventClick);
+                            }, 1);
+                        }
+
+                    }
+                }, false);
+            });
+        </script>
+        <?php
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# AdminerTsvToSql
-AdminerTsvToSql is a plugin for Adminer database tool that allows to insert data directly from Excel/OpenOffice/LibreOffice/etc using clipboard by automatically converting clipboard TSV (tab separated values) to SQL INSERT query. This is much more useful than saving and uploading CSV files. 
+AdminerTsvToSql - TSV to SQL plugin for Adminer
+===================
+
+`AdminerTsvToSql` is a **plugin for [Adminer database tool](http://www.adminer.org/)** that allows to insert data directly from Excel/OpenOffice/LibreOffice/etc using clipboard by automatically converting clipboard TSV (tab separated values) to SQL INSERT query.
+This is much more useful than saving and uploading CSV files.
+
+Allows copying/pasting TSV table data (from Excel, LibreOffice, etc) into Adminer SQL query field;
+Automatically detects TSV format and converts data into SQL query.
+First row should contain column names.
+
+![screenshot](screenshot.gif)
+   
+Plugin for Adminer database tool (http://www.adminer.org/).
+   
+## Usage
+Select and copy cells from Excel/OpenOffice/LibreOffice/etc
+Paste to SQL command field
+Change table name
+Profit!
+
+  
+# Installation
+
+1. [Download](http://www.adminer.org/#download) and install Adminer tool
+
+2. Create plugins folder next to `adminer.php`
+
+3. [Download `plugins.php`](https://raw.githubusercontent.com/vrana/adminer/master/plugins/plugin.php), this file is needed to load plugins; and place it into plugins folder
+
+4. [Download `AdminerTsvToSql.php`](https://github.com/Rastishka/AdminerTsvToSql/blob/master/AdminerTsvToSql.php) and place it into plugins folder
+
+	File structure will be:
+	```
+	- adminer.php
+	- plugins
+	    - plugin.php
+		- adminer-tables_fuzzy_search.php
+		- ... (other plugins you've already installed)
+	```
+
+5. Edit or create `index.php` file next to `adminer.php` and [configure plugins](http://www.adminer.org/plugins/#use) and add `new AdminerTsvToSql()` to $plugins array :
+
+```php
+<?php
+
+	function adminer_object()
+	{
+		// required to run any plugin
+		include_once "./plugins/plugin.php";
+
+		// autoloader
+		foreach (glob("plugins/*.php") as $filename) {
+			include_once "./$filename";
+		}
+
+		$plugins = array(
+			// specify enabled plugins here
+			new AdminerTsvToSql(),
+		);
+
+		return new AdminerPlugin($plugins);
+	}
+
+	// include original Adminer or Adminer Editor
+	include "./adminer.php";
+```
+
+Final file structure will be:
+```
+- adminer.php
+- index.php
+- plugins
+	- plugin.php
+	- AdminerTsvToSql.php
+	- ...
+```
+
